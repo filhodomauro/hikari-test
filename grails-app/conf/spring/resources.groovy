cab0a0ff330225035ebe52e1aad3f3b6a3a6b0f9
Mauro Filho <mauro.filho@gmail.com>
2016-07-15 07:48:37 -0300

    commit

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,6 +1,7 @@
+import org.grails.orm.hibernate.GrailsHibernateTransactionManager
 import org.springframework.transaction.PlatformTransactionManager
 
 // Place your Spring DSL code here
 beans = {
-    transactionManager_lookup(PlatformTransactionManager, ref("dataSource_lookup"))
+    transactionManager_lookup(GrailsHibernateTransactionManager)
 }
